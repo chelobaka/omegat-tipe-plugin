e8726f53b88d6a51458b1969f2cdd802b9ca5f42
Lev Abashkin <chelobaka@rocketmail.com>
2018-10-26 16:51:21 +0300

    Add tag shortcut insertion to context menu

--- src/main/java/net/scottie/tipe/PopupMenuConstructor.java
+++ src/main/java/net/scottie/tipe/PopupMenuConstructor.java
@@ -29,12 +29,19 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 import javax.swing.text.JTextComponent;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Editor popup menu constructor.
  */
 public class PopupMenuConstructor implements IPopupMenuConstructor {
+
+    private static final Pattern SHORTCUT_PAIR = Pattern.compile("(<([a-z]\\d+)>).+?(</\\2>)");
+    private static final Pattern SHORTCUT_SINGLE = Pattern.compile("<([a-z]\\d+)/>");
 
     /**
      * Wrap around selected text (if any) into a given tag.
@@ -69,8 +76,47 @@ public class PopupMenuConstructor implements IPopupMenuConstructor {
             return;
         }
 
+        String selection = Core.getEditor().getSelectedText();
+        if (selection == null) {
+            selection = "";
+        }
+
         JMenu pluginSubMenu = new JMenu();
         pluginSubMenu.setText(Util.RESOURCE_BUNDLE.getString("POPUP_MENU_NAME"));
+
+        /* Found shortcuts */
+        boolean shortcutsFound = false;
+        String src = Core.getEditor().getCurrentEntry().getSrcText();
+        Set<String> foundShortcuts = new HashSet<>();
+        Matcher matcher = SHORTCUT_PAIR.matcher(src);
+        while (matcher.find()) {
+
+            if (foundShortcuts.contains(matcher.group(2))) {
+                continue;
+            } else {
+                foundShortcuts.add(matcher.group(2));
+            }
+
+            JMenuItem item = new JMenuItem();
+            item.setText(matcher.group(1) + "…" + matcher.group(3));
+            String insertion = matcher.group(1) + selection + matcher.group(3);
+            item.addActionListener(e -> Core.getEditor().insertText(insertion));
+            pluginSubMenu.add(item);
+            shortcutsFound = true;
+        }
+
+        matcher = SHORTCUT_SINGLE.matcher(src);
+        while (matcher.find()) {
+            String shortcut = matcher.group(0);
+            JMenuItem item = new JMenuItem();
+            item.setText(shortcut);
+            item.addActionListener(e -> Core.getEditor().insertText(shortcut));
+            pluginSubMenu.add(item);
+            shortcutsFound = true;
+        }
+        if (shortcutsFound) {
+            pluginSubMenu.addSeparator();
+        }
 
         for (Map.Entry<String, String> entry : Util.TAG_MAP.entrySet()) {
             JMenuItem item = new JMenuItem();
